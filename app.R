93031529fccd4fba4e58779e74ce0a3af87d9826
Lizzie Pearmain <lizzie.pearmain@gmail.com>
2019-06-26 15:40:09 +0100

    v3.2
    Added functions for fail breeders.
    Currently showing everything in one pane in the phenology section as could not get tabsetPanels to work. This is fine for testing the functions but I will change the layout in future.

--- app.R
+++ app.R
@@ -1,8 +1,11 @@
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Shiny app to explore the methods in the density maps paper
-## Lizzie Pearmain & Ana Carneiro
-## June 2019
+## Copyright (C) 2019 Lizzie Pearmain & Ana Carneiro
+## This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+## This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+## You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 rm(list=ls())
 
@@ -24,9 +27,9 @@ ui <- navbarPage("Seabird phenology", selected = "Introduction",
   
   ## Link stylesheet and other HTML tags we need ----
   tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")#,
     # tags$link(href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap", rel="stylesheet")
-    tags$div(class = "footer", "Disclaimer: this app is not finished")
+    # tags$div(class = "footer", "Disclaimer: this app is not finished")
   ),
   
   
@@ -52,41 +55,83 @@ ui <- navbarPage("Seabird phenology", selected = "Introduction",
   ########################################################################################
   tabPanel("Phenology tables",
     titlePanel(h1("Seabird phenology")),
-  ## Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    ## Sidebar panel for inputs ----
-    sidebarPanel(tags$p("This tab uses mean laying date and mean lengths of the incubation, brood-guard and post-brood stages to estimate average start dates of each section of the breeding cycle."), tags$br(),    
-                 ## Input: Selector for choosing species type (this does nothing yet) ----
-                 selectInput(inputId = "species_type", label = "Choose whether your species is annual or biennial", choices = c("annual", "biennial")),
-                 ## Input mean length of pre-laying
-                 numericInput(inputId = "prelay_length_days", label = "Insert mean pre-laying length in days", value = 0),
-                 ## Input mean laying date as day-month
-                 textInput(inputId = "laydate_string", label = "enter mean laying date in format dd-mm", value = "01-01"),
-                 # Input: numeric input for length of incubation
-                 numericInput(inputId = "inc_length_days", label = "Insert mean incubation length in days", value = 25),
-                 # Input: numeric input for length of brood-guard
-                 numericInput(inputId = "brood_length_days", label = "Insert mean brood-guard length in days", value = 25),
-                 # Input: numeric input for length of post-brood
-                 numericInput(inputId = "post_length_days", label = "Insert mean post-guard length in days", value = 25),
-                 
-                 ## Action button to trigger calculation
-                 p("To calculate monthly phenology weightings, click the button below"),
-                 actionButton("go", "Go")
-    ),
-    
-    # Main panel for displaying outputs ----
-    mainPanel(
-      # # Output: Formatted text for caption ----
-      # h3(textOutput("caption", container = span)),
-      h2("Average phenology timings for your population:"),
-      # Output: HTML table with requested number of observations ----
-      tableOutput("phenTab1"),
-      tags$br(),
-      h2("Monthly average phenology for successful breeders:"),
-      tableOutput("phenTabBeta")
-      )
-    )
-  ),
+    ## Sidebar layout with input and output definitions ----
+    sidebarLayout(
+      ## Sidebar panel for inputs ----
+      sidebarPanel(tags$p("This tab uses mean laying date and mean lengths of the incubation, brood-guard and post-brood stages to estimate average start dates of each section of the breeding cycle."), tags$br(),    
+                   ## Input: Selector for choosing species type (this does nothing yet) ----
+                   # selectInput(inputId = "species_type", label = "Choose whether your species is annual or biennial", choices = c("annual", "biennial")),
+                   ## Input mean length of pre-laying
+                   numericInput(inputId = "prelay_length_days", label = "Insert mean pre-laying length in days", value = 0),
+                   ## Input mean laying date as day-month
+                   textInput(inputId = "laydate_string", label = "enter mean laying date in format dd-mm", placeholder = "dd-mm"), #value = "01-01",
+                   # Input: numeric input for length of incubation
+                   numericInput(inputId = "inc_length_days", label = "Insert mean incubation length in days", value = 25),
+                   # Input: numeric input for length of brood-guard
+                   numericInput(inputId = "brood_length_days", label = "Insert mean brood-guard length in days", value = 25),
+                   # Input: numeric input for length of post-brood
+                   numericInput(inputId = "post_length_days", label = "Insert mean post-guard length in days", value = 25),
+                   
+                   ## Action button to trigger calculation
+                   h3("To calculate monthly phenology weightings, click a button below:"),
+                   p("Successful breeders:"),
+                   # p("Adult sucessful breeders:") ## TODO: add options to calculate and display the timings for each pool of birds - as tabset isn't working.
+                   actionButton("goSB", "Go"),
+                   tags$br(),
+                   p("Fail breeders:"),
+                   # p("Adult sucessful breeders:") ## TODO: add options to calculate and display the timings for each pool of birds - as tabset isn't working.
+                   actionButton("goFB", "Go")
+                   
+      ),
+      
+      # Main panel for displaying outputs ----
+      mainPanel(#"Average phenology timings",
+          h2("Average phenology timings for successful breeders:"),
+          tableOutput("phenTab1Succ"),
+          tags$br(),
+          h3("Average fail date for breeders"),
+          p("(= halfway through breeding season from start of incubation to end of post-brood):"),
+          textOutput("failDate"),
+          tags$br(),
+          h2("Average phenology timings for fail breeders:"),
+          tags$br(),
+          tableOutput("phenTab1Fail"),
+          tags$br(),
+          h2("Monthly average phenology for successful breeders:"),
+          tableOutput("phenTabBeta"),
+          tags$br(),
+          h2("Monthly average phenology for fail breeders:"),
+          tableOutput("phenTabGamma"),
+          tags$br(), tags$br()
+        # tabsetPanel(
+        #   tabPanel("Successful breeders",
+        #            h2("Monthly average phenology for adult successful breeders (group beta):"),
+        #            tableOutput("phenTabBeta")
+        #   ),
+        #   tabPanel("Fail breeders",
+        #            h2("Average phenology timings for your population:"), tableOutput("phenTab1"), tags$br(),
+        #            h2("Monthly average phenology for adult fail breeders (group gamma):")#,
+        #            # tableOutput("phenTabDelta")
+        #   ),
+        #   tabPanel("Non-breeders",
+        #            h2("Average phenology timings for your population:"), tableOutput("phenTab1"), tags$br(),
+        #            h2("Monthly average phenology for adult non-breeders (sabbaticals) (group gamma):")#,
+        #            # tableOutput("phenTabGamma")
+        #   ),
+        #   tabPanel("Juveniles",
+        #            h2("Average phenology timings for your population:"), tableOutput("phenTab1"), tags$br(),
+        #            h2("Monthly average phenology for juveniles (group zeta):")#,
+        #            # tableOutput("phenTabTheta")
+        #   ),
+        #   tabPanel("Immatures",
+        #            h2("Average phenology timings for your population:"), tableOutput("phenTab1"), tags$br(),
+        #            h2("Monthly average phenology for immatures (group theta):")#,
+        #            # tableOutput("phenTabTheta")
+        #   )
+        # ) ## end tabsetPanel()
+      ) ## end mainPanel()
+    ) ## end sidebarLayout()
+  ), ## end tabPanel("Phenology")
   
   ########################################################################################
   ################### TAB 2 - DEMOGRAPHY MODEL ###########################################
@@ -126,19 +171,39 @@ server <- function(input, output) {
   ################## TAB 1 - PHENOLOGY TABLES ---- #######################################
   ########################################################################################
   
-  ## Make the first phenTable1 table----
-  makePhenTable1 <- reactive({
-    phenTable1(input$laydate_string, input$prelay_length_days, input$inc_length_days, 
+  #### Make tables for SUCCESSFUL BREEDERS ----
+  ## Make the first phenTable1 table
+  makePhenTable1Succ <- reactive({
+    phenTable1Succ(input$laydate_string, input$prelay_length_days, input$inc_length_days, 
                input$brood_length_days, input$post_length_days)
   })
-  
-  ## Make the pretty version of phenTable1 ----
-  makePrettyPhenTable1 <- reactive({
-    prettyPhenTable1(makePhenTable1())
+  ## Make the pretty version of phenTable1
+  makePrettyPhenTable1Succ <- reactive({
+    prettyPhenTable1(makePhenTable1Succ())
   })
   
-  ## Make the second phenTableBeta table ----
-  makePhenTableBeta <- eventReactive(input$go, {
+  #### Make output showing fail date as a text output ----
+  makeFailDate <- reactive({
+    findFailDate(makePhenTable1Succ())
+  })
+  ## Render it:
+  output$failDate <- renderText({
+    makeFailDate()
+  })
+  
+  #### Make tables for FAIL BREEDERS ----
+  ## Make the first phenTable1 table
+  makePhenTable1Fail <- reactive({
+    phenTable1Fail(input$laydate_string, input$prelay_length_days, input$inc_length_days, 
+                   input$brood_length_days, input$post_length_days)
+  })
+  ## Make the pretty version of phenTable1
+  makePrettyPhenTable1Fail <- reactive({
+    prettyPhenTable1(makePhenTable1Fail())
+  })
+  
+  ## Make the monthly table for SUCC - phenTableBeta table ----
+  makePhenTableBeta <- eventReactive(input$goSB, {
     
     ## create a progress object
     progress <- shiny::Progress$new()
@@ -154,17 +219,45 @@ server <- function(input, output) {
       progress$set(value = value, detail = detail)
     }
     
-    phenTableBeta(makePhenTable1(), updateProgress)
+    phenTableBeta(makePhenTable1Succ(), updateProgress)
   })
   
-  ## Render the first phenTable1 table ----
-  output$phenTab1 <- renderTable({
-    makePrettyPhenTable1()
+  ## Make the monthly table for SUCC - phenTableBeta table ----
+  makePhenTableGamma <- eventReactive(input$goFB, {
+    
+    ## create a progress object
+    progress <- shiny::Progress$new()
+    progress$set(message = "Computing table", value = 0)
+    ## close the progress when the reactive exits, even if there's an error:
+    on.exit(progress$close())
+    
+    updateProgress <- function(value = NULL, detail = NULL) {
+      if (is.null(value)) {
+        value <- progress$getValue()
+        value <- value + (progress$getMax() - value) / 5
+      }
+      progress$set(value = value, detail = detail)
+    }
+    
+    phenTableBeta(makePhenTable1Fail(), updateProgress)
   })
   
-  ## Render the second phenTableBeta table ----
+  ## Render the first phenTable1 table for SUCC ----
+  output$phenTab1Succ <- renderTable({
+    makePrettyPhenTable1Succ()
+  })
+  ## Render the first phenTable1 table for FAIL ----
+  output$phenTab1Fail <- renderTable({
+    makePrettyPhenTable1Fail()
+  })
+  
+  ## Render the second phenTableBeta table FOR SUCC ----
   output$phenTabBeta <- renderTable({
     makePhenTableBeta()
+  })
+  ## Render the second phenTableBeta table FOR FAIL ----
+  output$phenTabGamma <- renderTable({
+    makePhenTableGamma()
   })
   
   ########################################################################################
@@ -175,6 +268,12 @@ server <- function(input, output) {
   makeDemTable1 <- reactive({
     popStrat(input$afb, input$Sa, input$Sj, input$BS, input$BF)
   })
+  
+  ########################################################################################
+  ######### change output options
+  ########################################################################################
+  
+  # outputOptions(output, "phenTab1", suspendWhenHidden = FALSE)
   
 }
 
--- functions_phenology.R
+++ functions_phenology.R
@@ -1,8 +1,11 @@
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## Source functions for Density maps shiny app
-## Lizzie Pearmain & Ana Carneiro
-## June 2019
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## Phenology functions for Shiny app to explore the methods in the density maps paper
+## Copyright (C) 2019 Lizzie Pearmain & Ana Carneiro
+## This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+## This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+## You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 ###############################################################################
 ######## SECTION 1 - mini phenology table #####################################
@@ -38,8 +41,8 @@ ddmm2Display <- function(julian){
 }
 
 
-#### 1.3 Main function: create summary phenology table ----
-phenTable1 <- function(laydate_string, prelay_length_days, inc_length_days, brood_length_days, post_length_days){ ## TODO: add in , sp_type
+#### 1.3 Main function: create summary phenology table for SUCCESSFUL BREEDERS ----
+phenTable1Succ <- function(laydate_string, prelay_length_days, inc_length_days, brood_length_days, post_length_days){ ## TODO: add in , sp_type
   
   ## TESTING WITH PARAMS
   # laydate_string <- "29-07"
@@ -98,11 +101,111 @@ phenTable1 <- function(laydate_string, prelay_length_days, inc_length_days, broo
   df
 }
 
-## test
-# table <- phenTable1("10-01", 0, 70, 60, 40)
-# table
+#### 1.4 Helper function 3: find the fail date based on table of SUCC breeders NB. MIGHT NOT NEED THIS ----
+findFailDate <- function(table){
+  breed_interval <- interval(table[2,7], table[4,8]) ## interval from start of INC to end of PSB
+  fail_date <- breed_interval@start + as.duration(breed_interval)/2 + days(1) ## find failure date as mean of breed_interval: ex@start + as.duration(ex)/2
+  fail_date <- as.character(format(fail_date, "%d-%b"))
+  return(fail_date)
+}
 
-#### 1.4 Helper function: prettify the phenTable1 output for display ----
+#### 1.4. Helper function 4: convert inc_length_days, brood_length_days and post_length_days for fail breeders ----
+convertToFail <- function(vec, fail_day){
+  stopifnot(length(vec)==3) ## must be vector of length 3
+  xs <- cumsum(vec)
+  x2 <- xs ## create copy to work on
+  y <- fail_day
+  
+  if (y <= xs[1]){                                  ## fail during incubation
+    x2[1] <- y # x2[1] <- y - 1                     ## end inc on the day before failing
+    x2[2] <- 0                                      ## brg does not happen
+    x2[3] <- 0                                      ## psb does not happen
+  } else if (y > xs[1] & y <= xs[2]){               ## fail during brood-guard
+    x2[2] <- y - x2[1] # x2[2] <- y - 1 - x2[1]     ## end brg on the day before failing
+    x2[3] <- 0                                      ## psb does not happen
+  } else if (y > xs[2] & y <= xs[3]){               ## fail during post-guard
+    x2[3] <- y - x2[2] # x2[3] <- y - 1 - x2[2      ## end psb on the day before failing
+    x2[2] <- x2[2] - x2[1]                          ## convert brg back to brg rather than cumsum(inc, brg)
+  } else if (y > xs[3]){
+    stop("Fail date is not between start of incubation and end of post-brood.")
+  }
+  return(x2)
+}
+
+#### 1.5 Main function create summary phenology table for FAIL BREEDERS ----
+phenTable1Fail <- function(laydate_string, prelay_length_days, inc_length_days, brood_length_days, post_length_days){
+  
+  ## TESTING WITH PARAMS
+  # laydate_string <- "27-10" #"29-07"
+  # prelay_length_days <- 22 #10
+  # inc_length_days <- 68 #70
+  # brood_length_days <- 22 #60
+  # post_length_days <- 94 #40
+  
+  ## laydate is a character string of format "mm-dd" - convert to number
+  laydate <- ddmm2Julian(laydate_string)
+  
+  ## FIND START DATE OF WHOLE BREEDING CYCLE = pre-laying start date
+  start_date_julian <- laydate - prelay_length_days
+  
+  #### EXTRA SECTION FOR FAILS: REPLACE inc, brood, post WITH NEW VALUES AFTER FAILING HALFWAY THROUGH.
+  breed_length_days <- inc_length_days + brood_length_days + post_length_days
+  fail_day <- ceiling(breed_length_days/2) ## round up for consistency, and START NON-BREED ON THIS DAY NOW.
+  x <- c(inc_length_days, brood_length_days, post_length_days)
+  xf <- convertToFail(x, fail_day)
+  ## replace inc, brood and post with the new lengths
+  inc_length_days <- xf[1]
+  brood_length_days <- xf[2]
+  post_length_days <- xf[3]
+  rm(breed_length_days, fail_day, x, xf)
+  #### END EXTRA SECTION
+  
+  ## Create output data frame
+  df <- data.frame(matrix(ncol=10, nrow=5))
+  colnames(df) <- c("stage", "length", "start_jul_1", "end_jul_1", "start_jul_real", "end_jul_real", "start_date", "end_date", "start_display", "end_display")
+  df[ ,1] <- c("pre-laying", "incubation", "brood-guard", "post-brood", "non-breeding")
+  df
+  
+  ## fill in the length values
+  df[1,2] <- prelay_length_days
+  df[2,2] <- inc_length_days
+  df[3,2] <- brood_length_days
+  df[4,2] <- post_length_days
+  df[5,2] <- 365 - sum(prelay_length_days, inc_length_days, brood_length_days, post_length_days)
+  df
+  
+  ## fill in the table of START julian values from 1
+  df[1,3] <- 1
+  for (k in 1:4){
+    df[k+1,3] <- df[k,3] + df[k,2]
+  }
+  df
+  
+  ## fill in the table of END julian values
+  df[1,4] <- df[1,2]
+  for (m in 1:4){
+    df[m+1,4] <- df[m,4] + df[m+1,2]
+  }
+  df
+  
+  ## fill in the table of real julian values - just add (start_date_julian -1) to each start_jul_1 value.
+  df$start_jul_real <- df$start_jul_1 + start_date_julian - 1
+  df
+  df$end_jul_real <- df$end_jul_1 + start_date_julian - 1
+  df
+  
+  #### Part 2 - convert Julian dates to real dates over 2001 to 2002 by adding julian number to "2000-12-31"
+  df$start_date <- as.Date("2000-12-31", format="%Y-%m-%d") + df$start_jul_real
+  df$end_date <- as.Date("2000-12-31", format="%Y-%m-%d") + df$end_jul_real
+  
+  #### Part 3 - make pretty versions for start and end date for displaying in the table
+  df$start_display <- as.character(format(df$start_date, "%d-%b"))
+  df$end_display <- as.character(format(df$end_date, "%d-%b"))
+  
+  df
+}
+
+#### 1.6 Helper function: prettify the phenTable1 output for display ----
 
 prettyPhenTable1 <- function(table){
   output <- data.frame(matrix(ncol=4, nrow=5))
@@ -111,16 +214,30 @@ prettyPhenTable1 <- function(table){
   output$start <- table$start_display
   output$length <- as.integer(table$length)
   output$end <- table$end_display
+  ## only display end if length > 0
+  for (i in 1:nrow(output)) {
+    if (output$length[i] == 0) {
+      output$end[i] <- NA
+    }
+  }
   output
 }
 
-## test
+# ## test
+# # table <- phenTable1Succ("01-12", 0, 70, 60, 40)
+# table <- phenTable1Succ("27-10", 22, 68, 22, 94)
+# table
+# # tablef <- phenTable1Fail("01-12", 0, 70, 60, 40)
+# tablef <- phenTable1Fail("27-10", 22, 68, 22, 94)
+# tablef
+# 
 # prettyPhenTable1(table)
-
+# prettyPhenTable1(tablef)
+# findFailDate(table)
 
 
 ###############################################################################
-######## SECTION 2 - large phenology table ####################################
+######## SECTION 2 - large phenology tables ###################################
 ###############################################################################
 
 #### 2.1 Helper function 1: calculate number of days in intersect of two intervals ----
@@ -219,7 +336,11 @@ createEquation <- function(vec){
 # test
 # createEquation(vec)
 
-#### 2.4 Main function: monthly phenolog for BETA (successful breeders) ----
+
+
+###############################################################################
+#### 2.4 Main function: monthly phenology for BETA (successful breeders) ---- #
+###############################################################################
 
 ## input: phenTable1
 # table <- phenTable1("01-01", 0, 35, 5, 45)
@@ -342,14 +463,15 @@ phenTableBeta <- function(table, updateProgress=NULL){
 # table
 # phenTableBeta(table)
 
-### TESTING FUNCTIONS ----
-## phenology table 1
+# ### TESTING FUNCTIONS ----
+# ## phenology table 1
 # table <- phenTable1("29-07", 10, 70, 60, 40)
 # table
 # prettyPhenTable1(table)
 # 
-# ## phenologytable beta
+# # phenologytable beta
 # phenTableBeta(table)
-# 
+
 # interval2Months(prl_interval)
 # input <- prl_interval
+
